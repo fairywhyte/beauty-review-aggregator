d88a9650ff8e863c3f37105815e0c306d55a561c
Fairy <fadzilah.whyte@aol.co.uk>
2018-03-10 20:00:47 +0100

    Added product_description function into productcontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Product;
 use Illuminate\Http\Request;
 
+
 class ProductController extends Controller
 {
     /**
@@ -54,6 +55,47 @@ class ProductController extends Controller
         }
     }
 
+    public function description()
+    {
+        //$product_url =\App\Product::all(['product_url'])->toArray();
+        $product_url =\App\Product::first(['product_url']);
+        dd($product_url);
+        //Table::select('name','surname')->where('id', 1)->get();
+
+        //$html = file::get($product_url);
+   
+        $html = file_get_contents($product_url ); //get the html returned from the following url\
+
+        $sephora_serums_indiv_doc_page = new \DOMDocument();
+
+        libxml_use_internal_errors(TRUE); //disable libxml errors
+
+        if(!empty($html)){ //if any html is actually returned
+
+            $sephora_serums_indiv_doc_page->loadHTML($html);
+
+            libxml_clear_errors(); //remove errors for yucky html
+
+            $sephora_serums_indiv_xpath = new \DOMXPath($sephora_serums_indiv_doc_page);
+
+            //get all the product DESCRIPTIONS
+            $sephora_serums_description = $sephora_serums_indiv_xpath->query('//div[@class="css-8tl366"]');
+
+            //get all the product NUMBER OF RATINGS
+            $sephora_serums_nr_of_ratings = $sephora_serums_indiv_xpath->query('//button[@class="css-fisw11"]');
+
+            $product_desc_and_nr_of_ratings[] = [
+                $sephora_serums_description[0]->nodeValue,
+                $sephora_serums_nr_of_ratings[0]->nodeValue,
+            ];
+            $html = $sephora_serums_indiv_doc_page->saveHTML();
+
+        }
+
+        echo '<pre>';
+        print_r($product_desc_and_nr_of_ratings);
+        echo '</pre>';
+    }
 
     /**
      * Display the specified resource.
