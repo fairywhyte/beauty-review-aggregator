2222e1fcc836f79316de223a317c452b923f578d
Fairy <fadzilah.whyte@aol.co.uk>
2018-03-13 15:13:15 +0100

    dropped the shop_id and sephora_id from products table via php myadmin

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -38,19 +38,19 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-
         if (($handle = fopen ( public_path () . '/sephora_face_serum_product_attributes_1_405.csv', 'r' )) !== FALSE) {
             while ( ($data = fgetcsv ( $handle, 500, ',' )) !== FALSE ) {
 
                 $csv_data = new Product ();
-                $csv_data->brand = $data [0];
-                $csv_data->title = $data [1];
+                $csv_data->sephora_id = $data [0];
+                $csv_data->brand = $data [1];
+                $csv_data->title = $data [2];
               //$csv_data->average_rating = $data [2];
-                $csv_data->price = $data [3];
-                $csv_data->product_url = $data [4];
-                $csv_data->img135 = $data [5];
-                $csv_data->img250 = $data [6];
-                $csv_data->img450 = $data [7];
+                $csv_data->price = $data [4];
+                $csv_data->product_url = $data [5];
+                $csv_data->img135 = $data [6];
+                $csv_data->img250 = $data [7];
+                $csv_data->img450 = $data [8];
                 $csv_data->save ();
             }
             fclose ( $handle );
