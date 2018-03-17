42a35cbd66d332d5260bc789b40db929686c36a6
Unknown <zerayah@gmail.com>
2018-03-17 12:28:44 +0100

    half way (maybe) finished load_onfluenster function

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -24,25 +24,44 @@ class ServiceController extends Controller
         $influenster_products = [];
 
 
-        while ($row_infl = fgetcsv($fh_infl, 0, ','))
+        while ($row_infl = fgetcsv($fh_infl_r, 0, ','))
         {
 
             $key = str_slug($row_infl_r[0]);
 
             // try to find an item product_in_shop table with the same shop_id and the same id_in_shop
             $find_id_db = DB::select('SELECT shop_id FROM scraped_products');
-            $find_id_csv = substr($row_infl[4]);
+            //if i add the id into the csv file while scraping
+            $find_id_csv = substr($row_infl[6]);
 
-            // if it exists
-                // update it
-            // else
-                // first insert that item in product_in_shop
-                \App\Shop::INFLUENSTER_ID;
-                // try to find an item in product_in_shop that has the same slug
+            if($find_id_db == $find_id_csv){
                 // if it exists
-                    // set this new product_id to the other items product_id
+                    // update it
+
+            }else{
                 // else
-                    // create a new product
+                    // first insert that item in product_in_shop
+
+                $csv_data = new ProductIsInShop ();
+
+                $csv_data->title = $row_infl [0];
+                $csv_data->brand= $row_infl [1];
+                $csv_data->rating = $row_infl [2];
+                $csv_data->price = $row_infl[3];
+                $csv_data->scraped_at = $row_infl[4];
+                $csv_data->shop_id = $row_infl[6];
+                $csv_data->slug = $key;
+
+                $csv_data->save();
+
+                    \App\Shop::INFLUENSTER_ID;
+                    // try to find an item in product_in_shop that has the same slug
+                    // if it exists
+                        // set this new product_id to the other items product_id
+                    // else
+                        // create a new product
+
+            }
         }
         var_dump($influenster_products);
     }
