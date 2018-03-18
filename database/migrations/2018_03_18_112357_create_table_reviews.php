d3f678468b4a736c045fba6fc62d17eeb4d8faea
Mireille Bobbert <mlfbobbert@gmail.com>
2018-03-18 12:36:02 +0100

    added reviews table migration

---
+++ database/migrations/2018_03_18_112357_create_table_reviews.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableReviews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+
+    {
+        Schema::create('reviews', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('id_in_shop')->nullable();
+            $table->string('review_url')->nullable();
+            $table->text('review_text')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
