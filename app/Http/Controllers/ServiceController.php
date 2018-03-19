130c5e61fcd6d05c39438c011ae37dc13dd7e461
Unknown <zerayah@gmail.com>
2018-03-19 17:24:12 +0100

    putting influenster data into database, making some columns nullable

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ProductIsInShop;
+
 
 class ServiceController extends Controller
 {
@@ -13,57 +15,59 @@ class ServiceController extends Controller
 
     }
 
-    public function load_influenster()
+    public function load_influenster(Request $request)
     {
         //returns the fully qualified path to the project root.
-        $source_file_influenster = base_path('project_resources/fadz/influenster_scraping/influenster.csv');
+        $source_file_influenster = base_path('project_resources/fadz/influenster_scraping/influenster1-2.csv');
 
         $fh_infl_r = fopen($source_file_influenster, 'r');
-        $fh_infl_w = fopen($source_file_influenster, 'w');
+       // $fh_infl_w = fopen($source_file_influenster, 'w');
 
         $influenster_products = [];
 
+        while (!feof($fh_infl_r)) {
+        $row_infl = fgetcsv($fh_infl_r, 0, ',');
 
-        while ($row_infl = fgetcsv($fh_infl_r, 0, ','))
-        {
+        $rating = floatval(substr($row_infl[2], 0, strpos($row_infl[2], ' (')));
 
-            $key = str_slug($row_infl_r[0]);
 
-            // try to find an item product_in_shop table with the same shop_id and the same id_in_shop
-            $find_id_db = DB::select('SELECT shop_id FROM scraped_products');
-            //if i add the id into the csv file while scraping
-            $find_id_csv = substr($row_infl[6]);
+            $key = str_slug($row_infl[0]);
+            $csv_data = new ProductIsInShop ();
+            $csv_data->title = $row_infl [0];
+            $csv_data->brand= $row_infl [1];
+            $csv_data->rating = $rating;
+            $csv_data->price = $row_infl[3];
+            $csv_data->scraped_at = $row_infl[4];
+            $csv_data->shop_id = 2;
+            $csv_data->id_in_shop = uniqid();
+            $csv_data->slug = $key;
 
-            if($find_id_db == $find_id_csv){
-                // if it exists
-                    // update it
+            $csv_data->save();
 
-            }else{
-                // else
-                    // first insert that item in product_in_shop
+            // \App\Shop::INFLUENSTER_ID;
+            // try to find an item in product_in_shop that has the same slug
+            // if it exists
+            // set this new product_id to the other items product_id
+            // else
+            // create a new product
 
-                $csv_data = new ProductIsInShop ();
-
-                $csv_data->title = $row_infl [0];
-                $csv_data->brand= $row_infl [1];
-                $csv_data->rating = $row_infl [2];
-                $csv_data->price = $row_infl[3];
-                $csv_data->scraped_at = $row_infl[4];
-                $csv_data->shop_id = 2;
-                $csv_data->slug = $key;
-
-                $csv_data->save();
-
-                    // \App\Shop::INFLUENSTER_ID;
-                    // try to find an item in product_in_shop that has the same slug
-                    // if it exists
-                        // set this new product_id to the other items product_id
-                    // else
-                        // create a new product
-
-            }
         }
-        var_dump($influenster_products);
     }
 
 }
+
+
+
+// // try to find an item product_in_shop table with the same shop_id and the same id_in_shop
+// $find_id_db = DB::select('SELECT shop_id FROM scraped_products');
+// //if i add the id into the csv file while scraping
+// $find_id_csv = substr($row_infl[6]);
+
+// if($find_id_db == $find_id_csv){
+//     // if it exists
+//         // update it
+
+// }else{
+    // else
+        // first insert that item in product_in_shop
+        //}
---
+++ database/migrations/2018_03_19_160239_update_scraped_products_nullable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateScrapedProductsNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('scraped_products', function (Blueprint $table) {
+            //
+            $table->string("product_url")->nullable()->change();
+            $table->string("image450")->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('scraped_products', function (Blueprint $table) {
+            //
+        });
+    }
+}
