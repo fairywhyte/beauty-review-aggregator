cd19bddf0b86bfd7f134f27f21aaf5f51020e933
Mireille Bobbert <mlfbobbert@gmail.com>
2018-03-20 15:50:09 +0100

    updated home page, still in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,37 +4,39 @@
 
 
 
-<div class="container">
-    <h1>Where serums' best kept secrets get unraveled</h1>
-    <form class="form-inline my-lg-0">
-        <div>
-            <input class="form-control-lg form-control" type="search" placeholder="Enter product name, brand, eg 'Estee Lauder...'">
-        </div>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
+<div class="container mx-auto" >
+    <div id="searchbox-entry" class="container d-flex flex-column justify-content-start">
+        <h1>Where serums' best kept secrets get unraveled</h1>
+        <form class="form-inline my-2 my-lg-0 d-flex justify-content-center">
+            <input id="searchbox" class="form-control mr-sm-2" style="width: 120vh; height: 7vh" type="search" placeholder="Enter product name or brand, eg 'Drunk Elephant'..." aria-label="Search">
+            <button id="search-button" class="btn btn-outline-success my-2 my-sm-0" style="height: 7vh" type="submit">Search</button>
+        </form>
+    </div>
 
-    <div>Or search by...</div>
+    <div id="search-by" class="mt-2">Or search by...</div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm">
-                <div class="card" style="width: 18rem;">
+    <div class="container mt-3 ">
+        <div class="row ">
+            <div class="col-sm-12 col-lg-4">
+                <div class="card" style="width: 15rem;">
                     <img class="card-img-top" src="http://satyr.io/100x50" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Skin Concern</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm">
-                <div class="card" style="width: 18rem;">
+
+            <div class="col-sm-12 col-lg-4">
+                <div class="card" style="width: 15rem;">
                     <img class="card-img-top" src="http://satyr.io/100x50" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Brand Origin</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm">
-                <div class="card" style="width: 18rem;">
+
+            <div class="col-sm-12 col-lg-4">
+                <div class="card" style="width: 15rem;">
                     <img class="card-img-top" src="http://satyr.io/100x50" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Brand Name</p>
