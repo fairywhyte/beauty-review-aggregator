75986b89f7465b0493ded5ab1c36890c6548d221
Fairy <fadzilah.whyte@aol.co.uk>
2018-03-21 20:18:19 +0100

    added view for product detail page

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,1 +1,70 @@
-details
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" />
+
+
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Product Detail</title>
+
+    <style>
+
+    </style>
+</head>
+
+<body>
+@include('navbar')
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <h5 class="card-header">Product Detail</h5>
+                    <div class="card-img-top d-flex  flex-column flex-sm-row align-items-center">
+                        <div class="container mt-0">
+                             <img class="img-fluid img-detail" height="auto" src="{{URL::asset('/img/www.sephora.com--productimages-sku-s1468172-main-Lhero.jpg')}}" alt="EL_PoreMinimizingSkinRefresher" height="200" width="200"  class="image-responsive">
+                        </div>
+                        <div class="card-body m-auto">
+                            <p class="card-text col-md-8 p-2 m-0">Product Name : Idealist Pore Minimizing Skin Refresher</p>
+                            <p class="card-text col-md-8 p-2 m-0">Brand : Estée Lauder</p>
+                            <p class="card-text col-md-8 p-2 m-0">Brand Origin: France</p>
+                            <p class="card-text col-md-8 p-2 m-0">Price : $105</p>
+                            <p class="card-text col-md-8 p-2 m-0">Aggregated Rating : 4.0/5.0</p>
+                            <p class="card-text col-md-8 p-2 m-0">Aggregated Number of Reviews: 56</p>
+                            <p class="card-text col-md-8 p-2 m-0">What it is:
+                                    A fast-acting serum that deliver dramatic skin resurfacing benefits.
+                                    What it is formulated to do:
+                                    Feel it. See it. Have it: your ideal skin. This fast-acting serum resurfaces skin for an incredibly clear, even-toned, luminous complexion.
+                                    Idealist repairs the look of uneven skin texture, flakiness, and roughness while reducing the appearance of pores.
+                                    Smooth it on and instantly enjoy that soft, seamless, silky feeling you love.
+                                    Research results:
+                                    - 69% reduction in the look of pores in just 4 weeks.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
+
