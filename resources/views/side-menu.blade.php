7657b0b2621366df4ac4e82a107186a007ed24f8
Unknown <zerayah@gmail.com>
2018-03-22 09:43:24 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ resources/views/side-menu.blade.php
@@ -0,0 +1,210 @@
+<div class="navbar navbar-default visible-xs">
+  <div class="container-fluid">
+    <button class="btn btn-default navbar-btn" data-toggle="collapse" data-target="#filter-sidebar">
+      <i class="fa fa-tasks"></i> Toggle Sidebar
+    </button>
+  </div>
+</div>
+
+<div class="container-fluid">
+
+  <div class="row">
+
+    <!-- filter sidebar -->
+    <div id="filter-sidebar" class="col-xs-6 col-sm-3 visible-sm visible-md visible-lg collapse sliding-sidebar">
+
+      <div>
+        <h4 data-toggle="collapse" data-target="#group-1">
+          <i class="fa fa-fw fa-caret-down parent-expanded"></i>
+          <i class="fa fa-fw fa-caret-right parent-collapsed"></i>
+          Artist
+        </h4>
+        <div id="group-1" class="list-group collapse in">
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+        </div>
+      </div>
+
+      <div>
+        <h4 data-toggle="collapse" data-target="#group-2">
+          <i class="fa fa-fw fa-caret-down parent-expanded"></i>
+          <i class="fa fa-fw fa-caret-right parent-collapsed"></i>
+          Artist
+        </h4>
+        <div id="group-2" class="list-group collapse in">
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+        </div>
+      </div>
+
+      <div>
+        <h4 data-toggle="collapse" data-target="#group-3">
+          <i class="fa fa-fw fa-caret-down parent-expanded"></i>
+          <i class="fa fa-fw fa-caret-right parent-collapsed"></i>
+          Artist
+        </h4>
+        <div id="group-3" class="list-group collapse in">
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+        </div>
+      </div>
+
+      <div>
+        <h4 data-toggle="collapse" data-target="#group-4">
+          <i class="fa fa-fw fa-caret-down parent-expanded"></i>
+          <i class="fa fa-fw fa-caret-right parent-collapsed"></i>
+          Artist
+        </h4>
+        <div id="group-4" class="list-group collapse in">
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+          <a class="list-group-item" href="#">
+            <span class="badge">3</span> John Lennon
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- table container -->
+    <div class="col-sm-9">
+
+      <table class="table table-striped table-hover table-responsive">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th class="align-right">Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="valign-middle" data-label="Title">
+              <a href="#">See You Again</a>
+            </td>
+            <td class="valign-middle" data-label="Artist">
+              Wiz Khalifa
+            </td>
+            <td class="valign-middle align-right" data-label="Price">
+              $1.99
+            </td>
+            <td class="valign-middle align-right">
+              <a class="btn btn-default" href="#" data-toggle="tooltip" title="Add to Cart" data-placement="bottom" data-trigger="hover">
+                <i class="fa fa-cart-plus"></i>
+                <span class="visible-xs">Add to Cart</span>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td class="valign-middle" data-label="Title">
+              <a href="#">See You Again</a>
+            </td>
+            <td class="valign-middle" data-label="Artist">
+              Wiz Khalifa
+            </td>
+            <td class="valign-middle align-right" data-label="Price">
+              $1.99
+            </td>
+            <td class="valign-middle align-right">
+              <a class="btn btn-default" href="#" data-toggle="tooltip" title="Add to Cart" data-placement="bottom" data-trigger="hover">
+                <i class="fa fa-cart-plus"></i>
+                <span class="visible-xs">Add to Cart</span>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td class="valign-middle" data-label="Title">
+              <a href="#">See You Again</a>
+            </td>
+            <td class="valign-middle" data-label="Artist">
+              Wiz Khalifa
+            </td>
+            <td class="valign-middle align-right" data-label="Price">
+              $1.99
+            </td>
+            <td class="valign-middle align-right">
+              <a class="btn btn-default" href="#" data-toggle="tooltip" title="Add to Cart" data-placement="bottom" data-trigger="hover">
+                <i class="fa fa-cart-plus"></i>
+                <span class="visible-xs">Add to Cart</span>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td class="valign-middle" data-label="Title">
+              <a href="#">See You Again</a>
+            </td>
+            <td class="valign-middle" data-label="Artist">
+              Wiz Khalifa
+            </td>
+            <td class="valign-middle align-right" data-label="Price">
+              $1.99
+            </td>
+            <td class="valign-middle align-right">
+              <a class="btn btn-default" href="#" data-toggle="tooltip" title="Add to Cart" data-placement="bottom" data-trigger="hover">
+                <i class="fa fa-cart-plus"></i>
+                <span class="visible-xs">Add to Cart</span>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td class="valign-middle" data-label="Title">
+              <a href="#">See You Again</a>
+            </td>
+            <td class="valign-middle" data-label="Artist">
+              Wiz Khalifa
+            </td>
+            <td class="valign-middle align-right" data-label="Price">
+              $1.99
+            </td>
+            <td class="valign-middle align-right">
+              <a class="btn btn-default" href="#" data-toggle="tooltip" title="Add to Cart" data-placement="bottom" data-trigger="hover">
+                <i class="fa fa-cart-plus"></i>
+                <span class="visible-xs">Add to Cart</span>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+
+  </div>
+</div>
