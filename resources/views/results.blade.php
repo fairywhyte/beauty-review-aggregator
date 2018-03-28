1149088fbed340a8ee5f583042d9f50f43619b4c
Unknown <zerayah@gmail.com>
2018-03-28 15:27:37 +0200

    prices

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -37,62 +37,60 @@
             $brands_ids = array_keys($brands_count);
         @endphp
 
+
+
+
+
+
+
+
+
+        @php
+
+
+          $prices=[
+            0=>[],
+            1=>[],
+            2=>[]
+          ];
+
+          foreach($products as $product){
+            $productPrice = floatval(str_replace('$', '', $product->price));
+
+            if($productPrice<=50){
+                $prices[0][]=$productPrice;
+              }elseif($productPrice<=100){
+                $prices[1][]=$productPrice;
+              }else{
+                $prices[2][]=$productPrice;
+            }
+          }
+
+          foreach($prices as $range=>$rangeArr){
+              foreach($products as $product){
+                if($product->price<=$range){
+                  $range=$product->price;
+                  break;
+                }
+              }
+          }
+
+      @endphp
+
         <div id="group-1" class="list-group collapse in">
             {{-- @for( $i = 0; $i < count($brands_ids); $i++) --}}
             {{-- @for( $i = 0; $i < min(5, count($brands_ids)); $i++) --}}
 
             @for( $i = 0; $i < count($brands_ids); $i++)
                 <a class="list-group-item" href="#">
-                    <span class="badge badge-pill badge-secondary">{{ $brands_count[  $brands_ids[$i] ] }}</span> {{ $brands[ $brands_ids[$i] ]->name }}
+                    <span class="badge badge-pill badge-primary">{{ $brands_count[  $brands_ids[$i] ] }}</span> {{ $brands[ $brands_ids[$i] ]->name }}
                 </a>
             @endfor
 
         </div>
       </div>
 
-      <div>
-        <h4 class="side-bar-h4" id="h4Id2">
-          <i class="fa fa-fw fa-caret-down parent-expanded"></i>
-          <i class="fa fa-fw fa-caret-right parent-collapsed"></i>
-          Skin Type
-        </h4>
-        <div id="group-2" class="list-group collapse in">
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-        </div>
-      </div>
 
-      <div>
-        <h4 class="side-bar-h4" id="h4Id3">
-          <i class="fa fa-fw fa-caret-down parent-expanded"></i>
-          <i class="fa fa-fw fa-caret-right parent-collapsed"></i>
-          Price
-        </h4>
-        <div id="group-3" class="list-group collapse in">
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-        </div>
-      </div>
 
       <div>
         <h4 class="side-bar-h4" id="h4Id4">
@@ -101,18 +99,17 @@
           Ratings
         </h4>
         <div id="group-4" class="list-group collapse in">
+        @for( $i=0; $i < count($prices); $i++)
           <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
+
+            <span class="badge badge-pill badge-primary">{{count($prices[$i])}}
+              </span>
+            @if(count($prices[$i]) > 0)
+              {{ min($prices[$i])}} - {{max($prices[$i])}}
+            @endif
+            $
           </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
-          <a class="list-group-item" href="#">
-            <span class="badge">3</span> John Lennon
-          </a>
+          @endfor
         </div>
       </div>
 
@@ -150,7 +147,7 @@
                           <p class="price">{{$product->price}}</p>
                           <p class="average-rating">Average rating: {{number_format($product->average_rating,1)}}</p>
                           <p class="number-of-reviews">Number of reviews: {{number_format($product->total_number_of_ratings)}}</p>
-                          <a href="#" class="btn btn-secondary">View</a>
+                          <a href="#" class="btn btn-primary">View</a>
                     </div>
             </div>
           </div>
