6db090b0ea6427fffe0438ec6c08793ac5f22b60
Mireille Bobbert <mlfbobbert@gmail.com>
2018-03-28 15:46:59 +0200

    changed the margin bottom of the navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,6 +1,6 @@
 
 
-<nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light mb-5">
   <a class="navbar-brand" href="#">La Ravelle</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
