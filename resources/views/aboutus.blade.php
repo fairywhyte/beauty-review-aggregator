b09907b6c3a00de7180970524d7c6b615beaf835
Mireille Bobbert <mlfbobbert@gmail.com>
2018-03-30 11:39:24 +0200

    adapted style of the about us page and added FAQ

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -5,133 +5,94 @@
 <div class="container">
     <div class="aboutus">
         <h1 class="aboutus-h1">
-            ABOUT US
+            About Us and FAQ
             <br>
             <img src="https://image.ibb.co/nk616F/Layer_1_copy_21.png" width="47" height="11" align="center">
         </h1>
+        <h2 class="team-happen">The problem</h2>
         <article class="first-text">
             <p>
-
-
-
                 "It’s very difficult to find a beauty product that suits you and your skin. Sometimes you just end up with whatever is recommended
                 by the sales person. In order to find the right skincare product you have to test various ones based on your
                 findings. Rather than relying on the sales person who might be biased in recommending you a certain product,
                 we think the knowledge of all the women who have tried and tested skincare products is much more helpful to decide
                 which product is right for our skin. Therefore we decided to come up with La Ravelle, a beauty review aggregator
                 that sums up the number of ratings across various beauty websites, and gives back these aggregated numbers. In
-                this way, we help women cut the decision time to buy her next favourite skincare item.
+                this way, we help women cut the decision time to buy her next favourite skincare item."
             </p>
         </article>
         <br>
         <div class="aboutus-h1">
-
             <img src="https://image.ibb.co/nk616F/Layer_1_copy_21.png" width="47" height="11" align="center">
         </div>
 
 
+        <section id="team" class="two_fifth">
+            <h2 class="team-happen">The team who made it happen</h2>
+            <ul class="list-style">
+              <li class="team">
+                <figure class="clear"><img class="team-images" src="{{ asset('img/mireille.jpg') }}" alt="">
+                  <figcaption>
+                    <p class="team_name">Mireille Bobbert</p>
+                    <p class="team_description">I'm into skincare, word-of-mouth and scraping so this project was a no-brainer.</p>
+                  </figcaption>
+                </figure>
+              </li>
+              <li class="team">
+                <figure class="clear"><img class="team-images" src="{{ asset('img/fadz.JPG') }}" alt="">
+                  <figcaption>
+                    <p class="team_name">Fadz Whyte</p>
+                    <p class="team_description">I wanted to work on this tool because I knew it could solve my problems in buying skincare products.</p>
+                  </figcaption>
+                </figure>
+              </li>
+              <li class="team last-team">
+                <figure class="clear"><img class="team-images" src="{{ asset('img/eva.jpg') }}" alt="">
+                  <figcaption>
+                    <p class="team_name">Eva Kroon</p>
+                    <p class="team_description">I loved working with my team on this project as it was a lot of fun! Couldn't have asked for better teammates.</p>
+                  </figcaption>
+                </figure>
+              </li>
 
-
-
-
-
-<section id="team" class="two_fifth">
-        <h2 class="team-happen">The team who made it happen</h2>
-
-        <ul>
-          <li class="team">
-            <figure class="clear"><img src="{{ asset('img/mireille.jpg') }}" alt="">
-              <figcaption>
-                <p class="team_name">Mireille Bobbert</p>
-                <p class="team_title">Overlord</p>
-                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus.</p>
-              </figcaption>
-            </figure>
-          </li>
-          <li class="team">
-            <figure class="clear"><img src="{{ asset('img/fadz.JPG') }}" alt="">
-              <figcaption>
-                <p class="team_name">Fadz</p>
-                <p class="team_title">Overlords valued assistant</p>
-                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus.</p>
-              </figcaption>
-            </figure>
-          </li>
-          <li class="team last-team">
-            <figure class="clear"><img src="{{ asset('img/eva.jpg') }}" alt="">
-              <figcaption>
-                <p class="team_name">Eva Kroon</p>
-                <p class="team_title">Underling</p>
-                <p class="team_description">I loved working with my team on this project as it was a lot of fun! Couldn't have asked for better teammates.</p>
-              </figcaption>
-            </figure>
-          </li>
-
-        </ul>
-      </section>
-
-      <section class="faq last-team">
-          <h2 class="team-happen">Additional information about the project</h1>
-
-
-
-
-        <h4>Which websites do you scrape from?</h4>
-
-        <p>Sephora (https://www.sephora.com/) and Influenster (https://www.influenster.com/)</p>
-
-
-
-        <h4>What is an aggregate rating?</h4>
-
-        <p>Aggregated number of ratings per product = # of reviews Sephora + # of reviews Influenster</p>
-
-
-
-        <p>Aggregated average rating per product =</p>
-
-        <p>(Average rating Sephora * # of reviews Sephora + Average rating Influenster * # of reviews Influenster) /</p>
-
-        <p>Aggregated number of ratings per product</p>
-
-
-
-
-
-
-
-        <h4>What do we scrape?</h4>
-
-        <p>
-            Basic product information (title, brand name, price, av. rating + number of ratings)
-            Specific product information (product description)
-            Reviews from Sephora (last 100 reviews per product)
-            Recommended count percentage = the # of people who recommended the products /
-
-            the # of Sephora ratings for that product
-
-            Most helpful count percentage = the # of people who found a review helpful /
-
-            the # of Sephora ratings for that product
-        </p>
-
-
-
-
-
-
-
-        <h4>When do we match?</h4>
-
-        <p>When Sephora and Influenster show the same product on their website</p>
-
+            </ul>
         </section>
 
+        <section class="faq last-team">
+            <h2 class="team-happen">FAQ</h2>
+
+            <h4>Which websites do you scrape from?</h4>
+
+              <p><a href="https://www.sephora.com/" target="_blank">Sephora</a> and <a href="https://www.influenster.com/" target="_blank">Influenster</a></p>
+
+            <h4>What is an aggregate rating?</h4>
+
+              <p><b>Aggregated number of ratings per product</b> = # of reviews Sephora + # of reviews Influenster</p>
+
+              <p><b>Aggregated average rating per product</b> =</p>
+
+              <p>(Average rating Sephora * # of reviews Sephora + Average rating Influenster * # of reviews Influenster) /</p>
 
 
+            <h4>What do we scrape?</h4>
 
-</div>
+              <p>
+                <ul class="list-style">
+                    <li>Basic product information (title, brand name, price, av. rating + number of ratings)</li>
+                    <li>Specific product information (product description)</li>
+                    <li>Reviews from Sephora (last 100 reviews per product)</li>
+                    <ul class="list-style">
+                      <li><b>Recommended count percentage</b> = the # of people who recommended the products / the # of Sephora ratings for that product</li>
+                    </ul>
+                </ul>
+              </p>
 
+            <h4>When do we match?</h4>
+
+              <p>When Sephora and Influenster show <i>the same product</i> on their website.</p>
+
+        </section>
+    </div>
 </div>
 
 
