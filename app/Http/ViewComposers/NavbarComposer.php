00183ffd15400c49b2e4a748164c8f340dc924b2
Unknown <zerayah@gmail.com>
2018-03-30 15:02:16 +0200

    navbar composer

---
+++ app/Http/ViewComposers/NavbarComposer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\View\View;
+use App\Repositories\UserRepository;
+use App\Brand;
+
+class NavbarComposer
+{
+    public function compose(View $view)
+    {
+        //added the criteria for the input search,prices and brand into the array criteria
+        //add request method here to initialize the request to be used in this method
+        $request=request();
+        $criteria =[
+            'query'=> $request->input('query'),
+            'price'=>$request->input('price'),
+            'brand'=>$request->input('brand'),
+            'origin'=>$request->input('origin')
+
+        ];
+        //array_filter will remove some of the items in the array
+        $criteria = array_filter($criteria,function($value){
+            return $value!==null;
+        });
+
+        $view->criteria=$criteria;
+
+        //select from table brands where column equals origin
+        $origins = \DB::table('brands')->select('origin')->distinct()->orderBy('origin')->pluck('origin');
+        $view->origins=$origins;
+
+        $all_brands = \App\Brand::rightJoin('products','brands.id','brand_id')
+                ->select(\DB::raw('brands.*,count(products.id) as product_count'))
+                ->groupBy('products.brand_id')
+                ->get();
+
+                $view->all_brands=$all_brands;
+
+    }
+}
